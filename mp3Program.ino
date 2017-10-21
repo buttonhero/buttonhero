9f69284bd7ea7be2701242f396b787449aa5e79b
Michael <graymi@live.com>
2017-10-20 22:09:24 -0600

    Update mp3Program.ino

--- mp3Program.ino
+++ mp3Program.ino
@@ -1,16 +1,9 @@
 #include <Servo.h>
-
-//Demo for Serial MP3 Player A module
-//by Open-Smart Team and Catalex Team
-//catalex_inc@163.com
-//Board:  Arduino UNO R3
-//IDE:	  Arduino-1.6.4
-//Store: http://www.aliexpress.com/store/1199788
-//      http://dx.com
-//Demo Function: 
+#include <SPI.h>
+// 
 
 /* Wiring Guide */
-//Serial MP3 Player A     |  Arduino UNO R3//
+//Serial MP3 Player A     |  PINS//
 //              RX              |   7
 //              TX              |   8
 //              VCC            |   5V
@@ -38,11 +31,11 @@ static int8_t Send_buf[6] = {0} ;
 
 /*5 bytes commands*/
 #define CMD_SEL_DEV 0X35
-  #define DEV_TF 0X01
+#define DEV_TF 0X01
 #define CMD_IC_MODE 0X35
-  #define CMD_SLEEP   0X03
-  #define CMD_WAKE_UP 0X02
-  #define CMD_RESET   0X05
+#define CMD_SLEEP   0X03
+#define CMD_WAKE_UP 0X02
+#define CMD_RESET   0X05
 
 /*6 bytes commands*/  
 #define CMD_PLAY_W_INDEX   0X41
@@ -54,124 +47,118 @@ static int8_t Send_buf[6] = {0} ;
 #define CMD_PLAY_W_VOL 0X31
 
 #define CMD_SET_PLAY_MODE 0X33
-  #define ALL_CYCLE 0X00
-  #define SINGLE_CYCLE 0X01
+#define ALL_CYCLE 0X00
+#define SINGLE_CYCLE 0X01
 
 #define CMD_PLAY_COMBINE 0X45//can play combination up to 15 songs
 
-// Define Pins
-#define BLUE 3
-#define GREEN 5
-#define RED 6
+#define SW_PIN 2 // digital pin connected to switch output
+#define X_PIN 0 // analog pin connected to X output
+#define Y_PIN 1 // analog pin connected to Y output
+
+#define SONGS 5 //number of songs
 
 void sendCommand(int8_t command, int16_t dat );
 
 
 void setup() 
 {
-    //RGB LED Setup
-    pinMode(RED, OUTPUT); 
-    pinMode(GREEN, OUTPUT);
-    pinMode(BLUE, OUTPUT);
+
+  //joystick
+  pinMode(SW_PIN, INPUT_PULLUP);
+  digitalWrite(SW_PIN, HIGH);
+  Serial.begin(9600);
+  
   //serial baude rating for mp3 controller
 	myMP3.begin(9600);
 	delay(500);//Wait chip initialization is complete
     sendCommand(CMD_SEL_DEV, DEV_TF);//select the TF card
-    //Turn LED RED
-  analogWrite(RED, 255);
-  analogWrite(GREEN, 0);
-  analogWrite(BLUE, 0);  
-	delay(200);//wait for 200ms
-	
-  playWithVolume(0X0F0A);//3
 
-  
-  delay(700);
-  //Turn LED BLUE
-  analogWrite(RED, 0);
-  analogWrite(GREEN, 0);
-  analogWrite(BLUE, 255);
-  
-  playWithVolume(0X0F09);//2
-  delay(700);
-  //Turn LED GREEN
-  analogWrite(RED, 0);
-  analogWrite(GREEN, 255);
-  analogWrite(BLUE, 0);
-  
+  intro();
+
     
-  playWithVolume(0X0F08);//1
-  delay(350);
-  playWithVolume(0X0F05);//play song with volume 15(0x0F) class
-  delay(200);
-    // initialize digital pin LED_BUILTIN as an output.
-    digitalWrite(RED, HIGH);
-    digitalWrite(GREEN, LOW);
-    digitalWrite(BLUE, LOW);
 }
-
-// define variables
-int redValue;
-int greenValue;
-int blueValue;
-
+int isIntro = 1;
+int isPlaying = 1;
+int currSong = 1;
 void loop() 
 {
-#define delayTime 10 // fading time between colors
-
-redValue = 255; // choose a value between 1 and 255 to change the color.
-greenValue = 0;
-blueValue = 0;
-
-// this is unnecessary as we've either turned on RED in SETUP
-// or in the previous loop ... regardless, this turns RED off
-// analogWrite(RED, 0);
-// delay(1000);
-
-for(int i = 0; i < 255; i += 1) // fades out red bring green full when i=255
-{
-redValue -= 1;
-greenValue += 1;
-// The following was reversed, counting in the wrong directions
-// analogWrite(RED, 255 - redValue);
-// analogWrite(GREEN, 255 - greenValue);
-analogWrite(RED, redValue);
-analogWrite(GREEN, greenValue);
-delay(delayTime);
+if(digitalRead(SW_PIN) == LOW){
+  if(isIntro == 1){
+    playWithVolume(0X0F01);//play song with volume 15(0x0F) class
+    isPlaying = 1;
+    isIntro = 0;
+    delay(1000);    
+  }
+  else if(isPlaying == 1){
+    mp3Basic(CMD_PAUSE);
+    isPlaying = 0;
+    delay(1000);
+  }
+  else if(isPlaying == 0) {
+    mp3Basic(CMD_PLAY);
+    isPlaying = 1;
+    delay(1000);
+  }  
 }
 
-redValue = 0;
-greenValue = 255;
-blueValue = 0;
 
-for(int i = 0; i < 255; i += 1) // fades out green bring blue full when i=255
-{
-greenValue -= 1;
-blueValue += 1;
-// The following was reversed, counting in the wrong directions
-// analogWrite(GREEN, 255 - greenValue);
-// analogWrite(BLUE, 255 - blueValue);
-analogWrite(GREEN, greenValue);
-analogWrite(BLUE, blueValue);
-delay(delayTime);
+if(analogRead(X_PIN) > 800){
+  if(isIntro == 0 )
+  {
+    if(currSong < SONGS)
+    {
+      mp3Basic(CMD_NEXT_SONG);
+      currSong++;
+    }  
+    else
+    {
+    sendCommand(CMD_PLAY_FILE_NAME,0X0101);
+    currSong = 1;
+    }
+  delay(1000);
+  }
+}
+if(analogRead(X_PIN) < 200){
+  if(isIntro == 0 )
+  {
+  if(currSong > 1)
+  {
+    mp3Basic(CMD_PREV_SONG);
+    currSong--;
+  }  
+  else
+  {
+    sendCommand(CMD_PLAY_FILE_NAME,0X0105);
+    currSong = SONGS;
+  }
+  delay(1000);
+  }
+}
+if(analogRead(Y_PIN) > 800){
+  mp3Basic(CMD_VOLUME_UP);
+  delay(100);
+}
+if(analogRead(Y_PIN) < 200){
+  mp3Basic(CMD_VOLUME_DOWN);
+  delay(100);
 }
 
-redValue = 0;
-greenValue = 0;
-blueValue = 255;
-
-for(int i = 0; i < 255; i += 1) // fades out blue bring red full when i=255
-{
-// The following code has been rearranged to match the other two similar sections
-blueValue -= 1;
-redValue += 1;
-// The following was reversed, counting in the wrong directions
-// analogWrite(BLUE, 255 - blueValue);
-// analogWrite(RED, 255 - redValue);
-analogWrite(BLUE, blueValue);
-analogWrite(RED, redValue);
-delay(delayTime);
 }
+
+void intro()
+{
+  
+  setVolume(0X0F);
+  delay(200);
+  //sendCommand(CMD_SET_VOLUME,0x0F);
+  sendCommand(CMD_PLAY_FILE_NAME,0X0229);
+  delay(2000);
+  setVolume(0X0F);
+  delay(200);
+  sendCommand(CMD_PLAY_FILE_NAME,0X022A);
+  delay(200);
+  cyclePlay(0X022A);
 }
 
 void setVolume(int8_t vol)
@@ -269,4 +256,5 @@ void sendBytes(uint8_t nbytes)
     myMP3.write(Send_buf[i]) ;
   }
 }
+
 
